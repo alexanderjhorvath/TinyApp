efc0d5c18cda9b6ed8694f9f9e3f46c6fdcdae14
Alexander Horvath <tantousha@me.com>
2018-08-09 22:56:58 +0000

    completed task 8

--- express-server.js
+++ express-server.js
@@ -33,88 +33,54 @@ app.get("/", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  let templateVars = { users: users[req.cookies["user_id"]] };
+  let templateVars = { users: users[req.cookies.user_id] };
   res.render("register", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { users: users[req.cookies["user_id"]] };
+  let templateVars = { users: users[req.cookies.user_id] };
   res.render("login", templateVars);
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  if (req.body.email === "" || req.body.password === "") {
+    res.sendStatus(400);
+  }
+  let user;
+  for (var key in users) {
+    if (users[key].email === req.body.email) {
+    user = users[key].id;
+    }
+  }
+  if (user && users[key].password === req.body.password) {
+    res.cookie("user_id", user);
+    res.redirect("/urls");
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 app.post("/register", (req, res) => {
-  const {email, password} = req.body;
-  // for (var key in users) {
-  //   if (users[key].email === email) {
-  //     res.sendStatus(400)
-  //   } else {
-      let userID = generateRandomString();
-      createUser(userID, email, password);
-      res.cookie("user_id", userID);
-      res.redirect("/urls");
-  //   }
-  // }
+  if (req.body.email === "" || req.body.password === "") {
+    res.sendStatus(400);
+  }
+  for (var key in users) {
+    if (!users.hasOwnProperty(key)) continue;
+    if (users[key].email === req.body.email) {
+      res.sendStatus(400);
+    }
+  }
+  let userID = generateRandomString();
+  createUser(userID, req.body.email, req.body.password);
+  res.cookie("user_id", userID);
+  res.redirect("/urls");
 });
 
-//   lookupEmail(email);
-//   if (accountExists = false) {
-//     res.sendStatus(400);
-//     console.log("Account Exists: " + accountExists);
-//     console.log("Account Exists is the issue");
-//   } else if (email === "" || password === "") {
-//     res.sendStatus(400)
-//     console.log("else if is the issue");
-//   } else {
-//     let userID = generateRandomString();
-//     createUser(userID, email, password);
-//     res.cookie("user_id", userID);
-//     res.redirect("/urls");
-//   };
-// });
-
-//   for (let userID in users) {
-//     if (user[userID].email === email) {
-//   res.sendStatus(400);
-// } else if {
-
-// }
-//   }
-
-
-
-  // if (user) {
-  //   let userID = generateRandomString();
-  //   createUser(userID, email, password);
-  //   res.cookie("user_id", userID);
-  // } else if (email === "" || password === "") {
-  //   res.sendStatus(400);
-  // } else {
-  //   res.sendStatus(400);
-  // }
-  // if (req.body.email === "" || req.body.password === "") {
-  //   res.sendStatus(400);
-  // } else if (verifyAccount(req.body.email) === false) {
-  //   res.sendStatus(400);
-  // } else {
-  //   let userID = generateRandomString();
-  //   createUser(userID, req.body.email, req.body.password)
-  //   // console.log("Users: " + JSON.stringify(users));
-  //   res.cookie("user_id", userID);
-  //   res.redirect("/urls");
-
-
 app.get("/urls/new", (req, res) => {
-  let templateVars = { users: users[req.cookies["user_id"]] };
+  let templateVars = { users: users[req.cookies.user_id] };
   res.render("urls_new", templateVars);
 });
 
@@ -123,14 +89,12 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  //let id = req.cookies["user_id"];
-  let templateVars = { urls: urlDatabase, users: users[req.cookies["user_id"]] };
-  console.log(templateVars);
+  let templateVars = { urls: urlDatabase, users: users[req.cookies.user_id] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], users: users[req.cookies["user_id"]] };
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], users: users[req.cookies.user_id] };
   res.render("urls_show", templateVars);
 });
 
@@ -161,20 +125,6 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect("/urls");
 });
 
-function lookupEmail(newEmail) {
-  console.log("new email: " + newEmail);
-  var accountExists = false;
-  for (var key in users) {
-    console.log(users[key].email);
-    if (users[key].email !== newEmail) {
-      accountExists = false;
-      return;
-    } else {
-      accountExists = true;
-    }
-  }
-}
-
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x100000000).toString(36).substring(1);
 }
@@ -189,6 +139,7 @@ function updateURL(id, newURL) {
 }
 
 function createUser(id, email, password) {
+  console.log("Email: " + email + "Password: " + password);
   users[id] = {id: id, email: email, password: password};
 }
 
